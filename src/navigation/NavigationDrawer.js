ff50e80d90d607ff48e386465ee0c20bea87db70
davidmarint <maicolmarin0126@gmail.com>
2024-01-23 16:45:12 -0500

    estilizacion del drawer

--- src/navigation/NavigationDrawer.js
+++ src/navigation/NavigationDrawer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import  Icon  from 'react-native-vector-icons/FontAwesome5';
 import TapNavigationHome from './NavigationTapsHome';
 import TapNavigationCall from './NavigationTapsCall';
 import TapNavigationDirectory from './NavigationTapsDirectory';
@@ -8,8 +9,16 @@ const Drawer = createDrawerNavigator();
 
 export default function NavigationDrawer() {
     return(
-        <Drawer.Navigator>
-            <Drawer.Screen name="Inicio" component={TapNavigationHome}/>
+        <Drawer.Navigator screenOptions={{
+            headerShown: false, 
+            drawerActiveTintColor: '#ffffff',
+            drawerActiveBackgroundColor:'rgb(23,37,88)',}} >
+            <Drawer.Screen name="Inicio" component={TapNavigationHome} options={{
+                
+                drawerIcon: ({ color, size }) => (
+                    <Icon name="home" color={color} size={size}/>
+                ),
+            }}/>
             <Drawer.Screen name="Clases" component={TapNavigationCall}/>
             <Drawer.Screen name="Directorio" component={TapNavigationDirectory}/>
         </Drawer.Navigator>
--- src/navigation/stacks/NavigationStackHome.js
+++ src/navigation/stacks/NavigationStackHome.js
@@ -1,16 +1,43 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { View, Image } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import  Icon  from 'react-native-vector-icons/FontAwesome5';
 import Phone from '../../screens/Phone';
 import Home from '../../screens/Home';
 
 const Stack = createStackNavigator();
+//const navigation = useNavigation();
+
+const CustomHeaderTitle = () => (
+    <View style={{alignItems: 'center', width: 310, backgroundColor: 'red',}}>
+    <Image
+        source={require('../../assets/tituloHeader.png')} // Ruta de la imagen
+        style={{ width: 258, height: 55,}} 
+    />
+    </View>
+    );
 
 export default function NavigationClass() {
     return(
         <Stack.Navigator screenOptions={{
-            headerShown: false,
+            headerTintColor: 'rgb(23,37,88)',
+            headerStyle: {
+                backgroundColor: 'white',
+                height: 120,
+            },
+            headerTitle: () => <CustomHeaderTitle />,
         }}>
-            <Stack.Screen name="Principio" component={Home}/>
+            <Stack.Screen name="Principio" component={Home} options={{
+                headerLeft: () => {
+                    return(
+                        <Icon name="bars" 
+                        color="rgb(23,37,88)" 
+                        size={30} 
+                        //onPress={() =>navigation.dispatch(DrawerActions.openDrawer)}
+                        />
+                    )
+                }
+            }}/>
             <Stack.Screen name="Telefono" component={Phone}/>
             
         </Stack.Navigator>
